0027988feab04202e0c8ba03c283a3ae94eab001
Eduardo-Vitesse <edu.dev.py@gmail.com>
2021-09-01 16:37:46 -0300

    Menu desktop concluido

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,5 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+$(document).ready(function(){
+    $("#drop-down").click(function(){
+      $("#subMenuId").toggle(400);
+    });
+});

ee45a44fddb17b96790c49be149868ccba65b3f2
Eduardo-Vitesse <edu.dev.py@gmail.com>
2021-09-03 17:36:30 -0300

    Finalizando os slides responsívos

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
+// Função para ajustar responsividade global dos menus
 const menuResp = document.querySelector(".hidde-menu")
+const subMenuIdFooter = document.getElementById("subMenuId-footer")
 
 window.addEventListener('resize', () => {
   var windowWidth = window.innerWidth
   if(windowWidth > 800) {
     menuResp.style.display = 'none'
+    subMenuIdFooter.style.display = 'none'
   }
 })
 
+// Função de abrir o sropdown do menu principal
 $(document).ready(function(){
     $("#drop-down").click(function(){
       $("#subMenuId").toggle();
     });
 });
 
+// Funçãos do menu principal responsívos
 $(document).ready(function(){
   $("#menu-mobile").click(function(){
     $(".hidde-menu").toggle();
@@ -24,3 +29,12 @@ $(document).ready(function(){
     $("#subMenuId-mobile").toggle();
   });
 });
+
+// Função de abrir o sropdown do menu no rodapé
+$(document).ready(function(){
+  $("#drop-down-footer").click(function(){
+    $("#subMenuId-footer").toggle();
+  });
+});
+
+// Funçãos do menu no rodapé responsívos

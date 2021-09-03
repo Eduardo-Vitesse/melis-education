338933feaeb92a8c05df48f04cc3b6e1076392ae
Eduardo-Vitesse <edu.dev.py@gmail.com>
2021-09-03 12:35:41 -0300

    Finalizando o responsivo do menu superior

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
+const menuResp = document.querySelector(".hidde-menu")
+
+window.addEventListener('resize', () => {
+  var windowWidth = window.innerWidth
+  if(windowWidth > 800) {
+    menuResp.style.display = 'none'
+  }
+})
+
 $(document).ready(function(){
     $("#drop-down").click(function(){
-      $("#subMenuId").toggle(400);
+      $("#subMenuId").toggle();
     });
+});
+
+$(document).ready(function(){
+  $("#menu-mobile").click(function(){
+    $(".hidde-menu").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#drop-down-mobile").click(function(){
+    $("#subMenuId-mobile").toggle();
+  });
 });
